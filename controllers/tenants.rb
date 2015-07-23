88606f950c451d77bc0554523502dadb52968abe
Edward Park <parkes.edward@gmail.com>
2015-07-23 19:36:57 -0400

    added new tenant to apartment and redirected route

--- controllers/tenants.rb
+++ controllers/tenants.rb
@@ -1,11 +1,20 @@
 # index
 get '/apartments/:id/tenants' do
   @tenants = Apartment.find(params[:id]).tenants.all
-  binding.pry
   erb :"tenants/index"
 end
 
 # show
 get '/apartments/:id/tenants/new' do
+
   erb :"tenants/new"
+end
+
+#post
+post '/apartments/:id/tenants' do
+    binding.pry
+  new_tenants = Tenant.create(params[:tenant])
+
+  redirect"/apartments/#{new_tenants.apartment_id}"
+
 end
