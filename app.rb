9a6cf895b138881d8cc2abff96b44bb33ed22828
Erica Andersen <andersenek@gmail.com>
2015-07-23 16:53:19 -0400

    Added index route.

--- app.rb
+++ app.rb
@@ -6,3 +6,7 @@ require 'sinatra/reloader'
 require_relative 'controllers/welcome'
 require_relative 'controllers/apartments'
 require_relative 'controllers/tenants'
+
+get "/" do # Gets the path "/" index
+  erb :index # Finds index.erb template
+end
