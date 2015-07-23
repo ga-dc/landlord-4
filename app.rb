7e64fe987d2b83e1ca29f798310e7c8d269dc8a1
Erica Andersen <andersenek@gmail.com>
2015-07-23 17:20:59 -0400

    Created and connected database.

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
 
+# Load the file to connect to the DB
+require_relative 'db/connection.rb'
+
 # Load specific routes
 require_relative 'controllers/welcome'
 require_relative 'controllers/apartments'
@@ -14,4 +17,14 @@ end
 # New Route
 get "/apartments/new" do # This is the show form route for artists
   erb :"apartments/new" # Finds artists/index.erb template
+end
+
+# Show Route
+get "/apartments/:id" do # Gets the path "apartments/:id" ex. "/apartments/3/"
+  @artist = Artist.find(params[:id])
+  @artist_photo = @artist.photo_url
+
+  @artist_songs = Song.where(artist_id = @artist)
+
+  erb :"artists/show" # Finds artists/show.erb template, Shows apartment details
 end
