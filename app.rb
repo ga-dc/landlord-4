29806ae832ade6f810ea728ecf2f0aff09326f2a
Erica Andersen <andersenek@gmail.com>
2015-07-23 17:07:46 -0400

    Added new route to apartment form.

--- app.rb
+++ app.rb
@@ -8,5 +8,10 @@ require_relative 'controllers/apartments'
 require_relative 'controllers/tenants'
 
 get "/" do # Gets the path "/" index
-  erb :index # Finds index.erb template
+  erb :"apartments/index" # Finds index.erb template
+end
+
+# New Route
+get "/apartments/new" do # This is the show form route for artists
+  erb :"apartments/new" # Finds artists/index.erb template
 end
