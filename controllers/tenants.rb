a1bb50e5d9887ef7b38ab22b7b30b154132a5d6d
Matt Curtis <Curtismn87@yahoo.com>
2015-07-23 19:19:25 -0400

    Made a lot of changes

--- controllers/tenants.rb
+++ controllers/tenants.rb
@@ -1,9 +1,12 @@
 # index
 get '/apartments/:id/tenants' do
+  @apartment = Apartment.find_by(id:params[:id])
+  @tenants = @apartment.tenants
   erb :"tenants/index"
 end
 
 # show
 get '/apartments/:id/tenants/new' do
+  @apartment = Apartment.find_by(params[:id])
   erb :"tenants/new"
 end
