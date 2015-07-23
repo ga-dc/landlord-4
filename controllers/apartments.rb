fa87b413be09449bee4ba7480c14961565259dc6
Erica Irving <contact.eirving@gmail.com>
2015-07-23 17:23:31 -0400

    build out add new apartment

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -9,6 +9,11 @@ get '/apartments/new' do
   erb :"apartments/new"
 end
 
+post '/apartments' do
+  @apartment = Apartment.create(params[:apartment])
+  redirect "/apartments/#{@apartment.id}"
+end
+
 # show
 get '/apartments/:id' do
   @apartment = Apartment.find(params[:id])
