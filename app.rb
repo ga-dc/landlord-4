7b3992bc128aac1af22818bd71f12cb9a94db0f7
Erica Irving <contact.eirving@gmail.com>
2015-07-23 17:02:24 -0400

    link to and populate All Apartments from welcome

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 # Load specific routes
+require_relative 'db/connection.rb'
 require_relative 'controllers/welcome'
 require_relative 'controllers/apartments'
 require_relative 'controllers/tenants'
+#
+require_relative 'models/apartment'
+require_relative 'models/tenant'
