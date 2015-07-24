d7cb7b2767f511de3bfb9d2f583e02186b8775f1
Paul Doulatshahi <paul.doulatshahi@gmail.com>
2015-07-23 22:38:20 -0400

    homework done

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
 
+require_relative 'db/connection'
+require_relative 'db/seeds'
+
 # Load specific routes
 require_relative 'controllers/welcome'
 require_relative 'controllers/apartments'
 require_relative 'controllers/tenants'
+
+require_relative 'models/apartment'
+require_relative 'models/tenant'
