540db7a1d1a656351fdcf7c42d318e3e042e3182
Adrian Shepelavey <adrianshepelavey@gmail.com>
2015-07-23 23:43:43 -0400

    landlord-4 hw

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -1,14 +1,17 @@
 # index
 get '/apartments' do
+  @apartments = Apartment.all
   erb :"apartments/index"
-end
-
-# new
-get '/apartments/new' do
-  erb :"apartments/new"
 end
 
 # show
 get '/apartments/:id' do
+  @apartments = Apartment.find(params[:id])
   erb :"apartments/show"
+end
+
+# new
+post '/apartments' do
+  @apartments = Apartment.create!(params[:apartments])
+  redirect("/apartments/#{@apartments.id}")
 end
