190c4c322f4edbbad7d1ee7fc8b7497012a467d3
Lisa <lscottcook@hotmail.com>
2015-07-23 22:12:26 -0400

    saving homework

--- controllers/tenants.rb
+++ controllers/tenants.rb
@@ -1,9 +1,19 @@
 # index
 get '/apartments/:id/tenants' do
+  @apartment = Apartment.find(params[:id])
   erb :"tenants/index"
 end
 
 # show
 get '/apartments/:id/tenants/new' do
+  @apartment = Apartment.find(params[:id])
   erb :"tenants/new"
+end
+
+#post
+post '/apartments/:id/tenants' do
+  apartment = Apartment.find(params[:id])
+  apartment.tenants.create!(params[:apartment])
+  redirect "/apartments/#{apartment.id}/tenants"
+
 end
