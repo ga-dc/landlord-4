1d69ca7810212d5d77d17e9db0b373b431c4ee24
L Nawrocki <lwn2@aol.com>
2015-07-24 01:41:31 -0400

    mostly changes to view and controller files

--- app.rb
+++ app.rb
@@ -6,3 +6,10 @@ require 'sinatra/reloader'
 require_relative 'controllers/welcome'
 require_relative 'controllers/apartments'
 require_relative 'controllers/tenants'
+
+require_relative 'models/apartment'
+require_relative 'models/tenant'
+
+get '/' do
+  erb(:welcome)
+end
