63b4add5fe8a895ef89eeb23ce7cd9c9bc318875
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-23 22:12:12 -0400

    added show route

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -3,3 +3,9 @@ get "/apartments" do
   @apartments = Apartment.all
   erb :"apartments/index"
 end
+
+#show route
+get "/apartments/:id" do
+  @apartment = Apartment.find(params[:id])
+  erb :"apartments/show"
+end
