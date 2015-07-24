4e99988e6cd95359a52f684cd20d1d38d75b3db2
Rachel Warren <raelwa@gmail.com>
2015-07-24 08:29:25 -0400

    add apartments and add tenants

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -9,7 +9,14 @@ get '/apartments/new' do
   erb :"apartments/new"
 end
 
+#add new apartment form entries
+post "/apartments" do
+  @apartment = Apartment.create(params[:apartment])
+  redirect "/apartments/#{@apartment.id}"
+end
+
 # show
 get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
   erb :"apartments/show"
 end
--- controllers/tenants.rb
+++ controllers/tenants.rb
@@ -1,9 +1,22 @@
 # index
 get '/apartments/:id/tenants' do
+  @tenants = Apartment.find(params[:id]).tenants
   erb :"tenants/index"
+end
+
+# new
+get '/tenants/new' do
+  erb :"tenants/new"
+end
+
+#add new tenant form entries
+post "/tenants" do
+  @tenant = Tenant.create(params[:tenant])
+  redirect "/tenants/#{@tenant.id}"
 end
 
 # show
 get '/apartments/:id/tenants/new' do
+  @tenant = Tenant.find(params[:id])
   erb :"tenants/new"
 end
