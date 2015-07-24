fc99ab831d6acec0591a55858ca66eb528f74f6e
Erica Andersen <andersenek@gmail.com>
2015-07-24 09:11:13 -0400

    Updated controllers, added primary 'apartment' functionality, added CSS.

--- app.rb
+++ app.rb
@@ -3,28 +3,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 # Load the file to connect to the DB
-require_relative 'db/connection.rb'
+require_relative 'db/connection'
 
 # Load specific routes
 require_relative 'controllers/welcome'
 require_relative 'controllers/apartments'
 require_relative 'controllers/tenants'
 
-get "/" do # Gets the path "/" index
-  erb :"apartments/index" # Finds index.erb template
-end
-
-# New Route
-get "/apartments/new" do # This is the show form route for artists
-  erb :"apartments/new" # Finds artists/index.erb template
-end
-
-# Show Route
-get "/apartments/:id" do # Gets the path "apartments/:id" ex. "/apartments/3/"
-  @artist = Artist.find(params[:id])
-  @artist_photo = @artist.photo_url
-
-  @artist_songs = Song.where(artist_id = @artist)
-
-  erb :"artists/show" # Finds artists/show.erb template, Shows apartment details
-end
+# Load models
+require_relative 'models/tenant'
+require_relative 'models/apartment'
--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -1,14 +1,57 @@
+get "/" do # Gets the path "/" index
+  @all_apartments = "apartments/index"
+  @search_apartment = "apartments/search"
+  @show_apartment = "apartments/show"
+  @add_apartment = "apartments/new"
+  @list_tenants = "/apartments/3/tenants"
+  @add_tenant = "/apartments/3/tenants/new"
+  erb :"welcome" # Finds index.erb template
+end
+
 # index
-get '/apartments' do
-  erb :"apartments/index"
+get "/apartments" do # This is the index route for apartments
+  @header_route = "Apartment List"
+  @apartments = Apartment.all
+  erb :"apartments/index" # Finds index.erb template
 end
 
 # new
-get '/apartments/new' do
-  erb :"apartments/new"
+get "/apartments/new" do # This is the new form route for apartments
+  @header_route = "Add Apartment"
+  erb :"apartments/new" # Finds apartments/new.erb template
 end
 
 # show
-get '/apartments/:id' do
+get '/apartments/:id' do # This is the show route for apartments
+  @header_route = "Apartment"
+  @apartment = Apartment.find(params[:id])
   erb :"apartments/show"
+end
+
+# create
+post "/apartments" do
+  new_apartment = Apartment.create(params[:apartment])
+  redirect "/apartments/#{new_apartment.id}"
+end
+
+# edit
+get "/apartments/:id/edit" do
+  @header_route = "Edit Apartment"
+  @apartment = Apartment.find(params[:id])
+  erb :"apartments/edit"
+end
+
+# update
+put "/apartments/:id" do
+  @apartment_to_update = Apartment.find(params[:id])
+  @apartment_to_update.update(params[:apartment])
+  redirect "/apartments/#{@apartment_to_update.id}"
+end
+
+# destroy
+
+delete "/apartments/:id" do
+  @apartment_to_delete = Apartment.find(params[:id])
+  @apartment_to_delete.destroy
+  redirect "/apartments"
 end
