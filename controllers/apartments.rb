853b4147151300ac849b6ff3c7fd15869a4fbee6
Jeff Jones <jeff.jones1@gmail.com>
2015-07-23 20:41:10 -0400

    reconfiguring of layout and functionality working

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -1,5 +1,15 @@
+#sample
+get '/apartments/sample' do
+  erb :"apartments/sample"
+end
+
+get '/apartments/sample/tenants' do
+  erb :"apartments/sample_tenants"
+end
+
 # index
 get '/apartments' do
+  @apartments = Apartment.all
   erb :"apartments/index"
 end
 
@@ -8,7 +18,25 @@ get '/apartments/new' do
   erb :"apartments/new"
 end
 
+post '/apartments' do
+  @apartment = Apartment.create(params[:apartment])
+redirect "/apartments/#{@apartment.id}"
+end
+
 # show
 get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
   erb :"apartments/show"
+end
+
+# edit
+get '/apartments/:id/edit' do
+  @apartment = Apartment.find(params[:id])
+  erb :"apartments/edit"
+end
+
+put '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  @apartment.update(params[:apartment])
+  redirect '/apartments'
 end
