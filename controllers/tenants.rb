7f523ca4653ade870f7c5916c1098763a1a2691a
MikeDulik <mikedulik@gmail.com>
2015-07-24 09:00:45 -0400

    not finished, but gettin there

--- controllers/tenants.rb
+++ controllers/tenants.rb
@@ -1,9 +1,15 @@
 # index
 get '/apartments/:id/tenants' do
+  @tenants = Tenant.all.order(name: :asc)
   erb :"tenants/index"
 end
 
 # show
 get '/apartments/:id/tenants/new' do
   erb :"tenants/new"
+end
+
+post '/apartments/:id/tenants' do
+  @tenant = Tenant.create(params[:tenant])
+  redirect "/apartments/3/tenants"
 end
