9c9e2d008733cb5f695e2b80cd7ac20381b2ad30
Jeremy Koulish <jkoulish@gmail.com>
2015-07-24 10:12:12 -0400

    tied Sinatra to the DB using ActiveRecord

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -1,5 +1,8 @@
+require "pry"
+
 # index
 get '/apartments' do
+  @apartments = Apartment.all
   erb :"apartments/index"
 end
 
@@ -8,7 +11,19 @@ get '/apartments/new' do
   erb :"apartments/new"
 end
 
+post '/apartments' do
+  @apartment = Apartment.create(params[:apartment])
+  redirect :"apartments/#{@apartment.id}"
+end
+
 # show
 get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
   erb :"apartments/show"
+end
+
+delete '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  @apartment.destroy
+  redirect :"apartments/index"
 end
