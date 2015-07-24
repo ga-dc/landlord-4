979cb5f97eb24c627d529ecb78f9a73697409c18
ktw1222 <ktw1222@gmail.com>
2015-07-23 23:40:19 -0400

    landlord-4 hw

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
 
 # Load specific routes
 require_relative 'controllers/welcome'
 require_relative 'controllers/apartments'
 require_relative 'controllers/tenants'
+
+# Load models
+require_relative 'models/apartment'
+require_relative 'models/tenant'
+
+# connection to db
+require_relative 'db/connection'
--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -1,5 +1,6 @@
 # index
 get '/apartments' do
+  @apartments = Apartment.all
   erb :"apartments/index"
 end
 
@@ -8,7 +9,33 @@ get '/apartments/new' do
   erb :"apartments/new"
 end
 
+post '/apartments' do
+  @apartment = Apartment.create(params[:apartment])
+  redirect "/apartments/#{@apartment.id}"
+end
+
 # show
 get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
   erb :"apartments/show"
+end
+
+#Edit
+get '/apartments/:id/edit' do
+  @apartment = Apartment.find(params[:id])
+  erb :"apartment/edit"
+end
+
+#Update
+put '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  @apartment.update(params[:apartment])
+  redirect "/apartments/#{@apartment.id}"
+end
+
+#Delete
+delete '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  @apartment.destroy
+  redirect "/apartments"
 end
