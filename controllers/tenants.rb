7d109269ee5672643d8e0bd46fad5f2205df464d
lanak1301 <musiconstellation@gmail.com>
2015-07-23 23:16:03 -0400

    message

--- controllers/tenants.rb
+++ controllers/tenants.rb
@@ -1,9 +1,17 @@
-# index
+# show
 get '/apartments/:id/tenants' do
+  @apartment = Apartment.find(params[:id])
   erb :"tenants/index"
 end
 
-# show
+# new
 get '/apartments/:id/tenants/new' do
+  @apartment = Apartment.find(params[:id])
   erb :"tenants/new"
+end
+
+post '/apartments/:id/tenants' do
+  @apartment = Apartment.find(params[:id])
+  apartment.tenant.creates(params[:id])
+  redirect "/apartments/#{@apartment.id}/tenants"
 end
