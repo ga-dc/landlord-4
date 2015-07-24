7bda20d43fd32fe136f554ac632f45fa213ec174
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-23 21:35:39 -0400

    added relative req for connection. added get / route

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 # Load specific routes
+require_relative 'db/connection'
 require_relative 'controllers/welcome'
 require_relative 'controllers/apartments'
 require_relative 'controllers/tenants'
+
+#load general routes
+get "/" do
+  erb :welcome
+end
