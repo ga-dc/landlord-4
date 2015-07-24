6c6106baf49c53e5af518ee48effd712c996eddc
Cameron Fry <cameronfry11@gmail.com>
2015-07-24 00:42:36 -0400

    landlord-4 initial commit

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -1,5 +1,6 @@
 # index
 get '/apartments' do
+  @apartments = Apartment.all
   erb :"apartments/index"
 end
 
@@ -8,7 +9,24 @@ get '/apartments/new' do
   erb :"apartments/new"
 end
 
+post "/apartments" do
+  @apartment = Apartment.create!(params[:apartment])
+  redirect("/apartments/#{@apartment.id}")
+end
+
 # show
 get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
   erb :"apartments/show"
+end
+
+get "/apartments/:id/edit" do
+  @apartment = Apartment.find(params[:id])
+  erb :"apartments/edit"
+end
+
+put "/apartments/:id" do
+  @apartment = Apartment.find(params[:id])
+  @apartment.update(params[:apartment])
+  redirect "/apartments/#{@apartment.id}"
 end
