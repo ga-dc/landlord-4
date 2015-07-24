552fdb872ed4a1a5e02e4072013c4daa845f4a8e
Samuel Key <s.key08@gmail.com>
2015-07-24 07:24:49 -0400

    homework

--- app.rb
+++ app.rb
@@ -6,3 +6,7 @@ require 'sinatra/reloader'
 require_relative 'controllers/welcome'
 require_relative 'controllers/apartments'
 require_relative 'controllers/tenants'
+
+require_relative 'db/connection.rb'
+require_relative 'models/apartment'
+require_relative 'models/tenant'
