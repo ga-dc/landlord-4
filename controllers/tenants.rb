f9e285a5d5a8fe4c6191d85d624739c66104771f
Erik Anderson <itserikanderson@gmail.com>
2015-07-23 22:49:29 -0400

    added paths

--- controllers/tenants.rb
+++ controllers/tenants.rb
@@ -1,9 +1,11 @@
 # index
 get '/apartments/:id/tenants' do
+  @tenants = Tenant.all
   erb :"tenants/index"
 end
 
 # show
 get '/apartments/:id/tenants/new' do
+  @tenant = Tenant.create(params[:tenant])
   erb :"tenants/new"
 end
