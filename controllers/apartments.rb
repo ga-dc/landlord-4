cbeab63ed74b23ad9ef69f48a41f4b4d7ecce3b7
Silvia Brown <siltastic@gmail.com>
2015-07-24 00:29:15 -0400

    Added routes and edit placeholders.

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -1,14 +1,18 @@
-# index
 get '/apartments' do
+  @apartments = Apartment.all
   erb :"apartments/index"
 end
 
-# new
 get '/apartments/new' do
   erb :"apartments/new"
 end
 
-# show
+post '/apartments' do
+  @apartment = Apartment.create(params[:apartment])
+  redirect "/apartments/#{@apartment.id}"
+end
+
 get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
   erb :"apartments/show"
 end
