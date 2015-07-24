621ede0989b3bd7ba470f71e3e4779f4648230e3
Nicholas Olds <nolds9@yahoo.com>
2015-07-23 22:54:41 -0400

    Adding completed hw

--- controllers/tenants.rb
+++ controllers/tenants.rb
@@ -1,9 +1,41 @@
 # index
-get '/apartments/:id/tenants' do
+get '/tenants' do
+  @tenants = Tenant.all
   erb :"tenants/index"
 end
 
-# show
-get '/apartments/:id/tenants/new' do
+# new
+get '/tenants/new' do
   erb :"tenants/new"
+end
+# show
+get '/tenants/:id' do
+  @tenant = Tenant.find(params[:id])
+  erb :"tenants/show"
+end
+
+# create
+post '/tenants' do
+  @tenant = Tenant.create(params[:tenant])
+  redirect "/apartments/#{@tenant.id}"
+end
+
+# edit
+get '/tenants/:id/edit' do
+  @tenant = Tenant.find(params[:id])
+  erb :"tenants/edit"
+end
+
+# update
+put '/tenants/:id' do
+  @tenant = Tenant.find(params[:id])
+  @tenant.update(params[:tenant])
+  redirect "/apartments/#{@tenant.id}"
+end
+
+# destroy
+delete '/tenants/:id' do
+  @tenant = Tenant.find(params[:id])
+  @tenant.destroy
+  redirect "/tenants"
 end
