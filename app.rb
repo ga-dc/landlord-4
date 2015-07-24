7c2bd03712b0438f8aa8efad3b0b2bfcced94883
John Master <john.s.master@gmail.com>
2015-07-23 22:15:28 -0400

    set up controllers and views

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
 
+require_relative 'db/connection'
 # Load specific routes
 require_relative 'controllers/welcome'
 require_relative 'controllers/apartments'
 require_relative 'controllers/tenants'
+
+require_relative 'models/apartment'
+require_relative 'models/tenant'
--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -1,5 +1,7 @@
+require "pry"
 # index
 get '/apartments' do
+  @apartments = Apartment.all
   erb :"apartments/index"
 end
 
@@ -8,7 +10,13 @@ get '/apartments/new' do
   erb :"apartments/new"
 end
 
+post '/apartments' do
+  new_apartment = Apartment.create(params[:apartment])
+  redirect "/apartments/#{new_apartment.id}"
+end
+
 # show
 get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
   erb :"apartments/show"
 end
