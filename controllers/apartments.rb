fe3c7a74962d70f6da106e4b5579ad924dccbc82
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-23 22:22:37 -0400

    added new and create routes

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -4,6 +4,17 @@ get "/apartments" do
   erb :"apartments/index"
 end
 
+#new route
+get "/apartments/new" do
+  erb :"apartments/new"
+end
+
+#create route
+post "/apartments" do
+  new_apartment = Apartment.create(params[:apartment])
+  redirect "/apartments/#{new_apartment.id}"
+end
+
 #show route
 get "/apartments/:id" do
   @apartment = Apartment.find(params[:id])
